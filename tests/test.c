8ea46a9959c3912c8f2c5ae47ff38fd2ee17b27d
Tomaz <tomgm1502@gmail.com>
2024-03-08 13:09:26 +0100

    Making tests reproducible and adding edge cases

--- tests/test.c
+++ tests/test.c
@@ -8,11 +8,12 @@
 #include "../util.h"
 
 #define N_RANDOM_TESTS 1e5
+#define SEED 42
 
 void
 test_setup(void)
 {
-    srand(time(NULL));
+    srand(SEED);
 }
 
 void
@@ -23,26 +24,31 @@ test_tearDown(void)
 
 MU_TEST(roughly_bounds)
 {
-    int i;
-    char err_lower_bound[100] = {};
-    char err_upper_bound[100] = {};
+    int i, input, output, lower_bound, upper_bound;
+    char err_lower_bound[100];
+    char err_upper_bound[100];
 
     for (i = 0; i < N_RANDOM_TESTS; i++) {
-        int input = rand() % 1024;
-        /* if (rand() % 2) { */
-        /*     input = -input; */
-        /* } */
+        input = rand() % 1024;
+        if (rand() % 2) {
+            input = -input;
+        }
 
-        int output = roughly(input);
+        output = roughly(input);
+        lower_bound = 3 * input / 4;
+        upper_bound = 5 * input / 4;
 
         snprintf(err_lower_bound, sizeof(err_lower_bound),
             "Output of roughly function was too low. Input: %d / Output: %d.", input, output);
-        mu_assert(output >= 3 * input / 4, err_lower_bound); 
+        mu_assert(output >= lower_bound, err_lower_bound);
 
         snprintf(err_upper_bound, sizeof(err_upper_bound),
             "Output of roughly function was too high. Input: %d / Output: %d.", input, output);
-        mu_assert(output <= 5 * input / 4, err_upper_bound);
+        mu_assert(output <= upper_bound, err_upper_bound);
     }
+
+    mu_assert(roughly(1) == 1, "roughly(1) should be 1.");
+    mu_assert(roughly(0) == 0, "roughly(0) should be 0.");
 }
 
 MU_TEST_SUITE(babeld_tests)
