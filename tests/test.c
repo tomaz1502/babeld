79aaa74c77843344f614568bb2393e099845a6de
tomaz1502 <tomgm1502@gmail.com>
2024-03-19 14:27:40 +0100

    unit tests up to parse_thousands

--- tests/test.c
+++ tests/test.c
@@ -196,6 +196,131 @@ MU_TEST(timeval_compare_test)
     }
 }
 
+MU_TEST(timeval_min_test)
+{
+    struct timeval *s1, *s2;
+    int i, num_of_cases;
+
+    typedef struct test_case {
+        struct timeval s1_val;
+        struct timeval s2_val;
+        struct timeval expected;
+        const char* err_msg;
+    } test_case;
+
+    test_case tcs[] =
+    {
+        { {42, 10}, {42, 10}, {42, 10}, "timeval_min({42, 10}, {42, 10}) should be {42, 10}." },
+        { {42, 10}, {0, 0}, {42, 10}, "timeval_min({42, 10}, {0, 0}) should be {42, 10}." },
+        { {0, 0}, {42, 10}, {42, 10}, "timeval_min({0, 0}, {42, 10}) should be {42, 10}." },
+        { {42, 9}, {42, 10}, {42, 9}, "timeval_min({42, 9}, {42, 10}) should be {42, 9}." },
+        { {41, 15}, {42, 10}, {41, 15}, "timeval_min({41, 15}, {42, 10}) should be {41, 15}." },
+    };
+
+    num_of_cases = sizeof(tcs) / sizeof (test_case);
+
+    for(i = 0; i < num_of_cases; i++) {
+        s1 = &tcs[i].s1_val;
+        s2 = &tcs[i].s2_val;
+
+        timeval_min(s1, s2);
+
+        mu_assert(s1->tv_sec == tcs[i].expected.tv_sec, tcs[i].err_msg);
+        mu_assert(s1->tv_usec == tcs[i].expected.tv_usec, tcs[i].err_msg);
+    }
+}
+
+MU_TEST(timeval_min_sec_test)
+{
+    struct timeval *s;
+    time_t secs;
+    int i, num_of_cases;
+
+    typedef struct test_case {
+        struct timeval s_val;
+        time_t secs_val;
+        time_t secs_expected;
+        const char* err_msg;
+    } test_case;
+
+    test_case tcs[] =
+    {
+        { {42, 10}, 41, 41, "timeval_min_sec({42, 10}, 41) should be {41, _}." },
+        { {42, 10}, 43, 42, "timeval_min_sec({42, 10}, 41) should be {42, _}." },
+        // NOTE: Is it correct? Infinity shouldn't be just {0, 0}?
+        { {0, 10}, 1024, 1024, "timeval_min_sec({0, 10}, 1024) should be {1024, _}." },
+    };
+
+    num_of_cases = sizeof(tcs) / sizeof (test_case);
+
+    for(i = 0; i < num_of_cases; i++) {
+        s = &tcs[i].s_val;
+        secs = tcs[i].secs_val;
+
+        timeval_min_sec(s, secs);
+
+        mu_assert(s->tv_sec == tcs[i].secs_expected, tcs[i].err_msg);
+    }
+}
+
+MU_TEST(parse_nat_test)
+{
+    const char *string;
+    int result, i, num_of_cases;
+
+    typedef struct test_case {
+        const char * const string_val;
+        int expected;
+        const char* err_msg;
+    } test_case;
+
+    test_case tcs[] =
+    {
+        { "42", 42, "parse_nat(\"42\") should be 42." },
+        { "212348123481293", -1, "parse_nat(\"212348123481293\") should be -1." },
+        { "0", 0, "parse_nat(\"0\") should be 0." },
+    };
+
+    num_of_cases = sizeof(tcs) / sizeof (test_case);
+
+    for(i = 0; i < num_of_cases; i++) {
+        string = tcs[i].string_val;
+
+        result = parse_nat(string);
+
+        mu_assert(result == tcs[i].expected, tcs[i].err_msg);
+    }
+}
+
+MU_TEST(parse_thousands_test)
+{
+    const char *string;
+    int result, i, num_of_cases;
+
+    typedef struct test_case {
+        const char * const string_val;
+        int expected;
+        const char* err_msg;
+    } test_case;
+
+    test_case tcs[] =
+    {
+        { "42.1337", 42133, "parse_thousands(\"42.1337\") should be 42133." },
+        { "10.123456", 10123, "parse_thousands(\"10.123456\") should be 10123." },
+        { "0.1", 100, "parse_thousands(\"0.1\") should be 100." },
+    };
+
+    num_of_cases = sizeof(tcs) / sizeof (test_case);
+
+    for(i = 0; i < num_of_cases; i++) {
+        string = tcs[i].string_val;
+
+        result = parse_thousands(string);
+
+        mu_assert(result == tcs[i].expected, tcs[i].err_msg);
+    }
+}
+
 MU_TEST_SUITE(babeld_tests)
 {
     MU_SUITE_CONFIGURE(&test_setup, &test_tearDown);
@@ -204,6 +329,10 @@ MU_TEST_SUITE(babeld_tests)
     MU_RUN_TEST(timeval_minus_msec_test);
     MU_RUN_TEST(timeval_add_msec_test);
     MU_RUN_TEST(timeval_compare_test);
+    MU_RUN_TEST(timeval_min_test);
+    MU_RUN_TEST(timeval_min_sec_test);
+    MU_RUN_TEST(parse_nat_test);
+    MU_RUN_TEST(parse_thousands_test);
 }
 
 int
